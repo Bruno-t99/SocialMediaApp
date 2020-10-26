d6f2a9628edf87bdf17b755d4d5baeaa74020ff4
Bruno <fc51607@alunos.fc.ul.pt>
2020-10-26 17:12:11 +0000

    Logic domain finished

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,13 +1,19 @@
 const postResolvers = require("./posts")
 const usersResolvers = require("./users")
+const commentresolvers = require("./comments")
 
 module.exports ={
+    Post:{
+        likeCount: (parent) => {parent.likes.length},
+        commentCount: (parent) => {parent.comments.length}
+    },
     Query: {
         ...postResolvers.Query
     },
     Mutation: {
         ...usersResolvers.Mutation,
-        ...postResolvers.Mutation
+        ...postResolvers.Mutation,
+        ...commentresolvers.Mutation
     }
     
 }
--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,4 +1,4 @@
-const { AuthenticationError } = require("apollo-server");
+const { AuthenticationError, UserInputError } = require("apollo-server");
 const Post = require("../../models/Post")
 const checkAuth = require("../../utils/check-auth")
 
@@ -29,7 +29,7 @@ module.exports={
     Mutation: {
         async createPost(_,{body},context){
             const user = checkAuth(context);
-            console.log(user);
+            console.log("teste");
             const newPost = new Post({
                 body,
                 user: user.id,
@@ -57,6 +57,27 @@ module.exports={
       }else{
           return "User not found"
       }
-    }
+    },
+
+    async likePost(_,{postId}, context){
+        const {username} = checkAuth(context);
+        const post = await Post.findById(postId);
+        if(post){
+        if(post.likes.find(like => like.username === username)){
+            // post already liked,unlike it
+            post.likes = post.likes.filter(like => like.username != username)
+
+        }else{
+            //not liked, like post
+            post.likes.push({
+                username,
+                createdAt: new Date().toISOString()
+            })
+        }
+        await post.save();
+        return post;
+    }else throw new UserInputError("Post not found")
+
+}
 }
 }
